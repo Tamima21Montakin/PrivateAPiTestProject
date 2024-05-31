852dff4c68642cc9601e5b7dfb36e8eef3332fe9
Tamima21Montakin <tamima21montakin@gmail.com>
2024-05-31 16:54:47 +0600

    added generic resource csv files

--- test/readDataReturnRespV1.test.ts
+++ test/readDataReturnRespV1.test.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { readRequest } from '@util';
+
 // Define the type for the CSV data
 type CsvData = {
   resource: number;
@@ -27,14 +28,16 @@ function readCsvFile(csvFilePath: string): CsvData[] {
 
 }
 
-function processCsvFile(rows: CsvData[]): jsonData[] {
+// @ts-ignore
+function processCsvFileForSingleField(rows: CsvData[]): jsonData[] {
   
   const resultMap: { [key: string]: jsonData } = {};
 
   rows.forEach(row => {
     if (isNaN(row.resource) || isNaN(row.idList)) return;
+   
     // Check if there's already an entry for the current `resource` and `fields`
-    const key = `${row.idList}`;
+    const key = `${row.fields}`;
     if (!resultMap[key]) {
       // If not, create a new entry
       resultMap[key] = {
@@ -42,21 +45,48 @@ function processCsvFile(rows: CsvData[]): jsonData[] {
         idList: [row.idList],
         fields: row.fields
       };
-    } 
-    
-    // else {      
-    //   // for resource with multiple fields in a single request  
-    //   resultMap[key].fields += `,${row.fields}`;
-    // }
+    }
   });
 
   // Convert resultMap values to array
   return Object.values(resultMap);
 }
 
-const csvFilePath = path.join('D:/tasks/Generate CSV files for generic resource', 'job_res.csv'); 
+
+function processCsvFileForMultipleFields(rows: CsvData[]): jsonData[] {
+  
+    const resultMap: { [key: string]: jsonData } = {};
+  
+    rows.forEach(row => {
+      if (isNaN(row.resource) || isNaN(row.idList)) return;      
+   
+      // Check if there's already an entry for the current `resource` and `fields`
+      const key = `${row.idList}`;
+      if (!resultMap[key]) {
+        // If not, create a new entry
+        resultMap[key] = {
+          resource: row.resource,
+          idList: [row.idList],
+          fields: row.fields
+        };
+      }else {      
+        // for resource with multiple fields in a single request  
+        resultMap[key].fields += `,${row.fields}`;
+      }  
+  
+    });
+  
+    // Convert resultMap values to array
+    return Object.values(resultMap);
+  }
+// D:/tasks/Generate CSV files for generic resource
+const csvFilePath = path.join(__dirname, '..', 'scripts/files','job_res.csv');
+//path.join('.', 'job_res.csv'); 
 const getCsvdata = readCsvFile(csvFilePath);
-const jsonResult = processCsvFile(getCsvdata);
+//single fields
+//const jsonResult = processCsvFileForSingleField(getCsvdata);
+//multiple fields
+const jsonResult = processCsvFileForMultipleFields(getCsvdata);
 
 for(let i = 0; i <jsonResult.length; i++ )
       {
